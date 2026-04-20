1703fe3ce21139f69965cd8e0d059e0c3119d8e3
ThitikornC <tiice01@gmail.com>
2026-04-20 14:21:30 +0700

    Update index.html and sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // ================= Cache / Offline =================
-const CACHE_NAME = 'momay-cache-v2.18.1';
+const CACHE_NAME = 'momay-cache-v2.18.5';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
-  '/style.css?v=2.18.1',
-  '/script.js?v=2.18.1',
+  '/style.css?v=2.18.5',
+  '/script.js?v=2.18.5',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
